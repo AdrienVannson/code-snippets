9f26111a9cef579670b5a356f17eec4d84a2abf4
Adrien VANNSON <adrien.vannson@protonmail.com>
2018-11-02 17:52:03 +0100

    Directions

---
+++ snippets/Grille/directions_4.cpp
@@ -0,0 +1,2 @@
+const int NB_DIRECTIONS = 4;
+const int DELTAS_DIRECTIONS[NB_DIRECTIONS][2] = {{-1,0}, {0,-1}, {0,1}, {1,0}};
---
+++ snippets/Grille/directions_8.cpp
@@ -0,0 +1,2 @@
+const int NB_DIRECTIONS = 8;
+const int DELTAS_DIRECTIONS[NB_DIRECTIONS][2] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
