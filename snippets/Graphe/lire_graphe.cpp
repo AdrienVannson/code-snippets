640c12788ba23ebeb2d5f812f9e64780494f02c1
Adrien VANNSON <adrien.vannson@protonmail.com>
2018-11-02 16:15:05 +0100

    Lecture des graphes en dehors d'une fonction

--- snippets/Graphe/lire_graphe.cpp
+++ snippets/Graphe/lire_graphe.cpp
@@ -1,30 +1,26 @@
-void lireGraphe ()
-{
-    int nbArcs;
-    scanf("%d %d", &nbNoeuds, &nbArcs);
+int nbArcs;
+scanf("%d %d", &nbNoeuds, &nbArcs);
 
-    for (int iArc=0; iArc<nbArcs; iArc++) {
-        int source, cible;
-        scanf("%d %d", &source, &cible);
-        // source--, cible--;
+for (int iArc=0; iArc<nbArcs; iArc++) {
+    int source, cible;
+    scanf("%d %d", &source, &cible);
+    // source--, cible--;
 
 #ifdef GRAPHE_EST_PONDERE
-        int ponderation;
-        scanf("%d", &ponderation);
+    int ponderation;
+    scanf("%d", &ponderation);
 
-        noeuds[source].voisins.push_back(Arc{cible, ponderation});
+    noeuds[source].voisins.push_back(Arc{cible, ponderation});
 #else
-        noeuds[source].voisins.push_back(Arc{cible});
+    noeuds[source].voisins.push_back(Arc{cible});
 #endif
 
 #ifndef GRAPHE_EST_ORIENTE
 
 #ifdef GRAPHE_EST_PONDERE
-        noeuds[cible].voisins.push_back(Arc{source, ponderation});
+    noeuds[cible].voisins.push_back(Arc{source, ponderation});
 #else
-        noeuds[cible].voisins.push_back(Arc{source});
+    noeuds[cible].voisins.push_back(Arc{source});
 #endif
 #endif
-
-    }
 }
