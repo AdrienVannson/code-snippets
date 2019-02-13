5e8f5dba2583a892779e0fe4c24ce730d1f5e996
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-02-13 12:26:12 +0100

    Création d'un code C++ de départ

---
+++ snippets/c++/starter.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+using ll = long long;
+
+const int oo = 1000*1000*1000;
+
+
+int main ()
+{
+
+}
