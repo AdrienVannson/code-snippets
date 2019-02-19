44c891af5f7404f3f1fbf7206f763fc295ac315f
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-02-19 14:44:37 +0100

    BFS

---
+++ snippets/Graphe/BFS.cpp
@@ -0,0 +1,24 @@
+int dists[NB_MAX_NOEUDS];
+
+void BFS (const int iNoeud)
+{
+    fill(dists, dists+NB_MAX_NOEUDS, +oo);
+
+    queue< pair<int, int> > aVoir; // {iNoeud, dist}
+    aVoir.push( make_pair(iNoeud, 0) );
+
+    while (aVoir.size()) {
+        const int iNoeud = aVoir.front().first;
+        const int dist = aVoir.front().second;
+        aVoir.pop();
+
+        if (dists[iNoeud] != +oo) {
+            continue;
+        }
+        dists[iNoeud] = dist;
+
+        for (int voisin : noeuds[iNoeud].voisins) {
+            aVoir.push( make_pair(voisin, dist+1) );
+        }
+    }
+}
