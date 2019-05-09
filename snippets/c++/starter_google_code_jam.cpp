78a511b2670f7dd7291df03dece7cf504629bdf3
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-05-09 20:41:28 +0200

    Starter Google Code Jam

---
+++ snippets/c++/starter_google_code_jam.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+using ll = long long;
+
+const int oo = 1000*1000*1000;
+
+
+int main ()
+{
+    int nbTests;
+    scanf("%d", &nbTests);
+
+    for (int iTest=0; iTest<nbTests; iTest++) {
+
+
+        printf("Case #%d: \n", iTest+1);
+    }
+}
