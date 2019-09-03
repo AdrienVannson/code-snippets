6c0f3584b7d6ef299835441589e3b9a5336e2b6f
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-09-03 22:50:16 +0200

    Starter Battledev

---
+++ snippets/c++/starter_battledev.cpp
@@ -0,0 +1,21 @@
+#define LOCAL
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+struct ContestExerciseImpl { void main(); };
+int main() { ContestExerciseImpl().main(); }
+#else
+#include "exercise.hpp"
+ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
+#endif
+
+const int oo = 1000*1000*1000;
+
+
+void ContestExerciseImpl::main ()
+{
+
+}
