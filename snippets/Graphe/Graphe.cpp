9d4466546d5e8fd1efef07c67bae298e295ff824
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-10-23 18:34:53 +0200

    Graphe

--- snippets/Graphe/Graphe.cpp
+++ snippets/Graphe/Graphe.cpp
@@ -1,18 +1,10 @@
-#define GRAPHE_EST_ORIENTE
-#define GRAPHE_EST_PONDERE
-
 const int NB_MAX_NOEUDS;
-
 
 struct Arc
 {
     int cible;
-
-#ifdef GRAPHE_EST_PONDERE
-    int ponderation;
-#endif
+    // int ponderation;
 };
-
 struct Noeud
 {
     vector<Arc> arcs;
