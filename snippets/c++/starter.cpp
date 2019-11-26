4a8147af75fde8f2b85a52245cb6709c704ad19b
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-11-26 15:24:01 +0100

    Starter plus efficace

--- snippets/c++/starter.cpp
+++ snippets/c++/starter.cpp
@@ -1,15 +1,14 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <cstdio>
-
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
+#define for_n(i,n) for (int i=0;i<n;i++)
+inline int readInt () { int v; cin >> v; return v; }
 const int oo = 1000*1000*1000;
 
 
 int main ()
 {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
 
 }
