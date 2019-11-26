431089404588d1f3d6ab88bd213551dbdbf86260
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-11-26 15:13:19 +0100

    PGCD et coefs de Bezout

--- snippets/maths/PGCD_PPCM.cpp
+++ snippets/maths/PGCD_PPCM.cpp
@@ -1,9 +1,14 @@
 int PGCD (const int a, const int b)
 {
-    if (a == 0) {
-        return b;
-    }
+    if (a == 0) return b;
     return PGCD(b%a, a);
+}
+
+array<int,3> PGCD_bezout (const int x, const int y) // {pgcd, a, b} tels que ax+by=pgcd
+{
+    if (y == 0) return {x, 1, 0};
+    const array<int,3> suivant = PGCD_coef_bezout(y, x%y);
+    return {suivant[0], suivant[2], suivant[1] - (x/y)*suivant[2]};
 }
 
 int PPCM (const int a, const int b)
