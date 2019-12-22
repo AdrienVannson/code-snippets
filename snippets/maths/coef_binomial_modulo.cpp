8a2b35300d0f1a6b86cc63e19f86d159bb67e535
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-12-22 16:35:28 +0100

    Coefficients binomiaux

--- snippets/maths/coef_binomial_modulo.cpp
+++ snippets/maths/coef_binomial_modulo.cpp
@@ -1,51 +1,34 @@
 const int N_MAX;
 const int MODULO; // doit être premier
 
-ll factorielles[N_MAX];
+int facts[N_MAX]; // Premières factorielles
 
 void initialiserCoefsBinomiaux ()
 {
-    factorielles[0] = 1;
-    for (int i=1; i<N_MAX; i++) {
-        factorielles[i] = factorielles[i-1] * i % MODULO;
-    }
+    facts[0] = 1;
+    for (int i=1; i<N_MAX; i++) facts[i] = (ll)facts[i-1] * (ll)i % (ll)MODULO;
 }
 
-int xGCD (int a, int b, int &x, int &y)
+array<int,3> PGCD_coef_bezout (const int x, const int y) // {pgcd, a,b} tels que ax+by=pgcd
 {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
-    }
-
-    int x1, y1;
-    const int gcd = xGCD(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - (ll)(a / b) * y1;
-    return gcd;
+    if (y == 0) return {x, 1, 0};
+    const array<int,3> suivant = PGCD_coef_bezout(y, x%y);
+    return {suivant[0], suivant[2], suivant[1] - (x/y)*suivant[2]};
 }
 
-inline ll getInverse (const int a)
+inline int getInverse (const int x)
 {
-    int x, y;
-    xGCD(a, MODULO, x, y);
-    return x;
+    return PGCD_coef_bezout(x, MODULO)[1];
 }
 
 int getCoefBinomial (const int n, const int k)
 {
-    if (k < 0 || k > n) {
-        return 0;
-    }
-    if (k == 0 || k == n) {
-        return 1;
-    }
+    if (k < 0 || k > n) return 0;
+    if (k == 0 || k == n) return 1;
 
-    const int res = (factorielles[n] * getInverse(factorielles[k]) % MODULO) * getInverse(factorielles[n - k]) % MODULO;
+    const int res = ( (ll)facts[n]
+                    * (ll)getInverse(facts[k]) % (ll)MODULO)
+                    * (ll)getInverse(facts[n - k]) % (ll)MODULO;
 
-    if (res < 0) {
-        return res + MODULO;
-    }
-    return res;
+    return res < 0 ? res + MODULO : res;
 }
